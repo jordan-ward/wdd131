1b406ccdf5555814584b225d8c61a5f3e5b9126a
jordan-ward <fallbrook43@gmail.com>
2025-01-12 22:10:11 -0700

    homepage

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+const currentyear = document.querySelector("#currentyear");
+const lastmodified = document.querySelector("#lastmodified");
+
+const today = new Date();
+
+currentyear.innerHTML = `<span>${today.getFullYear()}</span>`;
+
+const date = new Date(document.lastModified);
+document.getElementById("lastmodified").innerHTML = date;
